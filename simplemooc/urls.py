be30e533c39d0edee2d9a897d541c28f5ecd46da
Felipe Emídio <felipe@labbitspace.com>
2020-05-09 19:11:41 -0300

    arquivo urls para o core

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from simplemooc.core import views
-
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contato/', views.contact, name='contact'),
+    path('', include('simplemooc.core.urls', namespace='core')),
     path('admin/', admin.site.urls),
 ]
