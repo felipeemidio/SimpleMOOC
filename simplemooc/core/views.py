ba1a9fbde8efcc7e83c3a8f815b6af97ac5ce2a6
Felipe Emídio <felipe@labbitspace.com>
2020-05-09 18:03:32 -0300

    primeiro template

--- simplemooc/core/views.py
+++ simplemooc/core/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the core index.")
+    return render(request, 'home.html', { 'usuario': 'Felipe' })
