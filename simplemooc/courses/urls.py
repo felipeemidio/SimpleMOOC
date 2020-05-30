f4432761b22a64b624ffeaf7d56849f5c7c57229
felipeemidio <lipe.emidio@gmail.com>
2020-05-29 23:53:43 -0300

    Adiciona get_absolute_url() em Course

--- simplemooc/courses/urls.py
+++ simplemooc/courses/urls.py
@@ -5,5 +5,5 @@ app_name = 'courses'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:pk>/', views.details, name='details'),
+    path('<slug>/', views.details, name='details'),
 ]
