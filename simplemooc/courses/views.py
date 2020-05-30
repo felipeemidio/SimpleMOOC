361677d8b2bca30da875692b588610a26e1bb33f
felipeemidio <lipe.emidio@gmail.com>
2020-05-30 12:48:49 -0300

    Formulário de contato em curso

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 
 
 def index(request):
@@ -14,5 +15,13 @@ def index(request):
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
     template_name = 'courses/details.html'
-    context = {'course': course}
+
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+    else:
+        form = ContactCourse()
+    context = {
+        'course': course,
+        'form': ContactCourse()
+    }
     return render(request, template_name, context)
